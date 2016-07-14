c94c5b5ef1b4558496390f77015d910e8d5e0388
dougachrist <dougachrist@gmail.com>
2016-07-14 16:26:45 -0700

    update article.js file to populate templat and fixed html for double quotes

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,6 +14,7 @@ Article.prototype.toHtml = function() {
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
+
   // DONE: If your template will use properties that aren't on the object yet, add them.
   //   Since your template can't hold any JS logic, we need to execute the logic here.
   //   The result is added to the object as a new property, which can then be referenced
@@ -24,6 +25,10 @@ Article.prototype.toHtml = function() {
 
   // TODO: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  return template(this);
+
 };
 
 ourLocalData.sort(function(a,b) {
